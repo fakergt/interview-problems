98a3864751feb794e363f47f3b6bfcb400949686
Veaceslav Lazari <lazariveaceslav@gmail.com>
2021-07-28 12:25:45 +0300

    array duplicate and string reverse problem

---
+++ src/com/company/ArrayDuplicate.java
@@ -0,0 +1,67 @@
+package com.company;
+
+public class ArrayDuplicate {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 1, 2, 3, 2, 5};
+
+        System.out.println("Initial array :");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n removed duplicates");
+        for (int i : returnArray(array)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] returnArray(int[] array) {
+        int[] anotherArray = array;
+        for (int i = 0; i < anotherArray.length; i++) {
+            for (int j = 0; j < anotherArray.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (anotherArray[i] == anotherArray[j]) {
+                    anotherArray = removeTheElement(anotherArray, j);
+                }
+            }
+        }
+        return anotherArray;
+    }
+
+    public static int[] removeTheElement(int[] arr,
+                                         int index) {
+
+        // If the array is empty
+        // or the index is not in array range
+        // return the original array
+        if (arr == null
+                || index < 0
+                || index >= arr.length) {
+
+            return arr;
+        }
+
+        // Create another array of size one less
+        int[] anotherArray = new int[arr.length - 1];
+
+        // Copy the elements except the index
+        // from original array to the other array
+        for (int i = 0, k = 0; i < arr.length; i++) {
+
+            // if the index is
+            // the removal element index
+            if (i == index) {
+                continue;
+            }
+
+            // if the index is not
+            // the removal element index
+            anotherArray[k++] = arr[i];
+        }
+
+        // return the resultant array
+        return anotherArray;
+    }
+}
---
+++ src/com/company/StringReverse.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        System.out.println("Add a string to reverse: ");
+        String string = new Scanner(System.in).nextLine();
+
+        System.out.println(reverse(string));
+    }
+
+    public static String reverse(String string){
+        StringBuilder reverse = new StringBuilder();
+        for (int i=string.length(); i>0; i--){
+            reverse.append(string.charAt(i-1));
+        }
+        return reverse.toString();
+    }
+}

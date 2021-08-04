94d42d2070ff3216c2606700d7afd8d421bdf4af
Veaceslav Lazari <lazariveaceslav@gmail.com>
2021-08-04 13:03:18 +0300

    leap year

---
+++ src/com/company/LeapYear.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        System.out.println("Current year is :" + year);
+        System.out.println("Add your year of choice: ");
+        int secondYear = new Scanner(System.in).nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("year " + year + " is a Leap Year");
+        } else {
+            System.out.println("year " + year + " it's not a Leap year");
+        }
+
+        if (isLeapYear(secondYear)) {
+            System.out.println("year " + secondYear + " is a Leap Year");
+        } else {
+            System.out.println("year " + secondYear + " it's not a Leap year");
+        }
+    }
+
+    private static boolean isLeapYear(int year) {
+        return year % 4 == 0 && year % 100 != 0;
+    }
+}

6decbfc6908ca0a6a8269037fa7c53cc9aa83d27
Veaceslav Lazari <lazariveaceslav@gmail.com>
2021-08-08 13:07:56 +0300

    reverse number

---
+++ src/com/company/ReverseNumber.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.println("Add your number: ");
+        int number = new Scanner(System.in).nextInt();
+
+        System.out.println(returnReverseNumber(number));
+    }
+
+    private static int returnReverseNumber(int number) {
+        int n = number;
+        int reverse = 0;
+        while (n != 0) {
+            int remains = n % 10;
+            reverse = reverse * 10 + remains;
+            n = n / 10;
+        }
+        return reverse;
+    }
+}

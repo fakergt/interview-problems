ce9cf3b858bc6f2fe3c8b1cf5311c8476dddb8e7
Veaceslav Lazari <lazariveaceslav@gmail.com>
2021-08-19 14:33:02 +0300

    binary search

--- src/main/java/com/interview/math/Fibonacci.java
+++ src/main/java/com/interview/math/Fibonacci.java
@@ -2,9 +2,10 @@ package com.interview.math;
 
 public class Fibonacci {
     public static void main(String[] args) {
-//        for (int i = 1; i < 20; i++) {
-//            System.out.print(fibonacci(i) + " ");
-//        }
+        for (int i = 1; i < 10; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        System.out.println();
         fibonacci2();
     }
 
--- src/main/java/com/interview/math/PrimeNumbers.java
+++ src/main/java/com/interview/math/PrimeNumbers.java
@@ -5,16 +5,33 @@ public class PrimeNumbers {
     public static void main(String[] args) {
 
         System.out.print("1 2 3");
-        for (int i=4; i<20;i++){
-            int count =0;
-            for (int j=2;j<i;j++){
-                if (i%j==0){
+        for (int i = 4; i < 20; i++) {
+            int count = 0;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
                     count++;
                 }
             }
-            if (count==0){
-                System.out.print(" "+i);
+            if (count == 0) {
+                System.out.print(" " + i);
             }
         }
+
+        System.out.print("\n1 ");
+        for (int i = 0; i < 20; i++) {
+            if (i!=0 && i!=1 && isPrime(i)) {
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        int sqrt = (int) Math.sqrt(number) + 1;
+        for (int i = 2; i < sqrt; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
